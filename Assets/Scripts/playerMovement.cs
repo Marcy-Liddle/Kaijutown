dc12b3d59bad268d2384c6032c85fedf3cc83a0f
Marcy-Liddle <2400826@uad.ac.uk>
2025-12-14 20:35:19 +0000

    Work on integrating laser into movement scene
    Needs more explosion behaviour, committing to work on player model

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -60,11 +60,6 @@ public class playerMovement : MonoBehaviour
             jump();
         }
 
-        if (Input.GetKeyDown("Fire"))
-        {
-            DoRaycast();
-        }
-
     }
 
     void FixedUpdate()
@@ -121,48 +116,5 @@ public class playerMovement : MonoBehaviour
         }
     }
 
-    void DoRaycast()
-    {
-        RaycastHit hitInfo; //gives us information about what we hit (if anything)
-        Ray ray = new Ray(transform.position, -transform.up);
-
-        //Do the raycast. Store the information in hitInfo
-        m_RayHit = Physics.Raycast(ray, out hitInfo, m_RayDistance);
-
-        if (m_RayHit)
-        {
-            m_HitPoint = hitInfo.point;     //Store the position that our ray collided with the object
-            m_HitNormal = hitInfo.normal;   //Store the surface normal of the object
-            m_Grounded = Vector3.Dot(Vector3.up, hitInfo.normal) > 0.5f; //Bit of a magic number here. Just trust me on this one.
-
-            Instantiate(explode, m_HitPoint, transform.rotation);
-
-        }
-
+   
     }
-
-
-
-    private void OnDrawGizmos()
-    {
-        if (m_RayHit)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(transform.position, m_HitPoint);
-
-            Gizmos.color = Color.green;
-            Gizmos.DrawSphere(m_HitPoint, 0.125f);
-
-            Gizmos.color = Color.blue;
-            Gizmos.DrawLine(m_HitPoint, m_HitPoint + m_HitNormal);
-        }
-        else
-        {
-            //Draw a line to where the ray ends
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(transform.position, transform.position - transform.up * m_RayDistance);
-
-            Gizmos.DrawSphere(transform.position - transform.up * m_RayDistance, 0.125f);
-        }
-    }
-}
