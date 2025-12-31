b45b0cd042cc67f9607c1ed1cb754bbf64c4564e
Marcy-Liddle <2400826@uad.ac.uk>
2025-12-31 17:07:07 +0000

    Dash action creates an object that briefly appears attached to the body hierarchy as part of the attack

---
+++ Assets/Scripts/dashBehaviour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class dashBehaviour : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = new Vector3(1.11f, - 10.28f, 0.56f);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public float moveSpeed = 10f;
     private float moveHorizontal;
-	private float moveForward;
+    private float moveForward;
 
     //Jumping physics
     public float jumpForce = 10f;
@@ -31,6 +31,7 @@ public class PlayerController : MonoBehaviour
     public GameObject dash;
 
     [SerializeField] private GameObject head;
+    [SerializeField] private GameObject body;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -39,7 +40,7 @@ public class PlayerController : MonoBehaviour
 
         //cameraTransform = Camera.main.transform;
         //cameraTransform.rotation = body.transform.rotation;
-     
+
 
         // Hides the mouse
         Cursor.lockState = CursorLockMode.Locked;
@@ -47,15 +48,15 @@ public class PlayerController : MonoBehaviour
 
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true; //stops the player from rotating and getting stuck on the floor when trying to move
-        
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-		moveHorizontal = Input.GetAxisRaw("Horizontal");
-		moveForward = Input.GetAxisRaw("Vertical");
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveForward = Input.GetAxisRaw("Vertical");
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -63,10 +64,10 @@ public class PlayerController : MonoBehaviour
             jump();
         }
 
-        if (Input.GetKeyDown("enter"))
+        if (Input.GetKeyDown("tab"))
         {
-            Debug.Log("Dash");
-            dashingAttack();
+
+            attack();
         }
 
     }
@@ -74,7 +75,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         MovePlayer();
-        
+
         ApplyJumpPhysics();
     }
 
@@ -90,12 +91,12 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0, horizontalRotation, 0);
 
         verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
-       
+
         verticalRotation = Mathf.Clamp(verticalRotation, -35f, 45f);
 
         head.transform.localRotation = Quaternion.Euler(0, 0, verticalRotation);
         //cameraTransform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
-      
+
 
     }
 
@@ -110,24 +111,24 @@ public class PlayerController : MonoBehaviour
         velocity.x = targetVelocity.x;
         velocity.z = targetVelocity.z;
         rb.linearVelocity = velocity;
-        
+
         //if player is moving (and no other stepping sound is playing), play a random stomp sound effect
         if ((velocity.x != 0 || velocity.z != 0) & source.isPlaying == false)
         {
             AudioClip step = stomps[(int)Random.Range(0, stomps.Length)];
             source.clip = step;
             source.PlayOneShot(step, 1.0f);
-        
-        }   
+
+        }
     }
 
-    
 
-    void jump() 
+
+    void jump()
     {
         rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z); // Initial burst for the jump
 
-        
+
     }
 
     void ApplyJumpPhysics()
@@ -136,7 +137,7 @@ public class PlayerController : MonoBehaviour
         {
             // Falling: Apply fall multiplier to make descent faster
             rb.linearVelocity += Vector3.up * Physics.gravity.y * fallMultiplier * Time.fixedDeltaTime;
-        } // Rising
+        }
         else if (rb.linearVelocity.y > 0)
         {
             // Rising: Change multiplier to make player reach peak of jump faster
@@ -144,29 +145,28 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void dashingAttack()
-    {
-        float temp = moveSpeed;
-        moveSpeed = 20f;   
-        dashing(moveSpeed, temp);
-    }
-   
-    IEnumerator dashing(float moveSpeed, float temp)
+
+
+    void attack()
     {
         Vector3 movement = ((transform.right * moveHorizontal) + transform.forward * moveForward).normalized;
-        Vector3 targetVelocity = movement * moveSpeed;
-
-        // Apply movement to the Rigidbody
+        Vector3 targetVelocity = movement * moveSpeed * 2;
         Vector3 velocity = rb.linearVelocity;
         velocity.x = targetVelocity.x;
-        velocity.z = targetVelocity.z;
+        velocity.z = 0f;
         rb.linearVelocity = velocity;
 
-        rb.linearVelocity = velocity;
-        Instantiate(dash, transform.position, transform.rotation);
-        yield return new WaitForSeconds(2f);
-        Destroy(dash);  
-        
-        
+
+        GameObject dashing = null;
+
+        if (dashing == null)
+        {
+
+            dashing = Instantiate(dash, parent:body.transform, false );
+
+            Destroy(dashing, 2f);
+        }
+
     }
-    }
+
+}
